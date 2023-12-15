077922df046e9f267d52be30bc3fb038e4137815
Ogordiunor Uche Jude <excelxz78@gmail.com>
2023-12-14 21:12:11 -0800

    second commit

--- add_node.c
+++ add_node.c
@@ -4,23 +4,26 @@
  * @head: A pointer to the top mode node of a stack_t linked list.
  * @n: The value to be stored in the new node.
  */
-void addnode(stack_t **head, int n) {
-    stack_t *new_node = (stack_t *)malloc(sizeof(stack_t));
-    if (new_node == NULL) {
-        fprintf(stderr, "Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
+void addnode(stack_t **head, int n)
+{
+stack_t *new_node = (stack_t *)malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+fprintf(stderr, "Memory allocation failed.\n");
+exit(EXIT_FAILURE);
+}
 
-    new_node->n = n;
-    new_node->next = *head;
-    new_node->prev = NULL;
+new_node->n = n;
+new_node->next = *head;
+new_node->prev = NULL;
 
-    if (*head != NULL) {
-        (*head)->prev = new_node;
-    }
+if (*head != NULL)
+{
+(*head)->prev = new_node;
+}
 
-    *head = new_node;
+*head = new_node;
 
-    printf("Added %d to the stack.\n", n);
+printf("Added %d to the stack.\n", n);
 }
 
